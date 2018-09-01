e9a6f8c67a983e931e6ccc7aff9dd7a0afb974d2
Rishav Bhagat <rishavb123@gmail.com>
2018-08-31 22:08:41 -0400

    created a script for the night

--- scripts/night.py
+++ scripts/night.py
@@ -1,5 +1,17 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, db
+import datetime
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("../credentials/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
+
+def getHour():
+    return int(str(datetime.datetime.time(datetime.datetime.now())).split(":")[0])
+
+curHour = -1
+
+while True:
+    if getHour() == 1 and curHour != 1:
+        db.reference('SetTemp').set(80)
+    elif getHour() == 9 and curHour != 9:
+        db.reference('SetTemp').set(77)
