4dcc7c76c37865b36dc2f7359659d5bffcee9ab3
Rishav Bhagat <rishavb123@gmail.com>
2018-08-31 22:22:40 -0400

    added finishing touches

--- scripts/night.py
+++ scripts/night.py
@@ -3,15 +3,31 @@ from firebase_admin import credentials, db
 import datetime
 
 cred = credentials.Certificate("../credentials/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://nest-controller.firebaseio.com/'
+})
 
 def getHour():
     return int(str(datetime.datetime.time(datetime.datetime.now())).split(":")[0])
 
 curHour = -1
 
+print "\nThis Script will set the temperature to 80 at 1AM and 77 at 9AM"
+print "Use Ctrl C to stop the script\n\n"
+print "Running . . ."
+
 while True:
-    if getHour() == 1 and curHour != 1:
-        db.reference('SetTemp').set(80)
-    elif getHour() == 9 and curHour != 9:
-        db.reference('SetTemp').set(77)
+    try:
+        if getHour() == 1 and curHour != 1:
+            db.reference('SetTemp').set(80)
+            print "Set Temperature to 80 Degrees\n"
+            print "Running . . ."
+        elif getHour() == 9 and curHour != 9:
+            db.reference('SetTemp').set(77)
+            print "Set Temperature to 80 Degrees\n"
+            print "Running . . ."
+        curHour = getHour()
+    except KeyboardInterrupt:
+        print "\n\nScript stopped"
+        print "Goodbye"
+        break
