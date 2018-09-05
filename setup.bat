8367dc5f4b7c731097dd110e8344156fee495320
Rishav Bhagat <rishavb123@gmail.com>
2018-09-05 00:07:59 -0400

    batch script to open all relevant content to start working on this project

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+index.html
+chrome "https://github.com/rishavb123/nest" "https://console.firebase.google.com/u/7/project/nest-controller/overview"
+cd scripts
+start
+start pycharm .
+cd ..
+explorer .
+atom .
