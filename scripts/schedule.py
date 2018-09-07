ff2156cd429775b9288efe4e172ce3840c43a71b
Rishav Bhagat <rishavb123@gmail.com>
2018-09-06 23:20:36 -0400

    minor changes

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -13,11 +13,13 @@ def get_hour():
 
 
 # def get_hour():
-#     return int(str(db.reference('TestHour').get()))
+#     h = int(str(db.reference('TestHour').get()))
+#     print h
+#     return h
 
 
-def check(num):
-    return get_hour() == num and curHour != num
+def check(num, cur_hour):
+    return cur_hour == num and curHour != num
 
 
 def set_temp(temp):
@@ -28,17 +30,27 @@ def set_temp(temp):
 
 curHour = -1
 
-print "\nThis Script will set the temperature to 80 at 1AM and 77 at 9AM"
+print "\nThis Script will set the temperature to a specified schedule"
 print "Use Ctrl C to stop the script\n\n"
 print "Running . . ."
 
 while True:
     try:
-        if check(1):
+
+        hour = get_hour()
+
+        if check(1, hour):
             set_temp(80)
-        elif check(8):
+        elif check(6, hour):
             set_temp(77)
-        curHour = get_hour()
+        elif check(8, hour):
+            set_temp(83)
+        elif check(19, hour):
+            set_temp(80)
+        elif check(20, hour):
+            set_temp(77)
+        curHour = hour
+
     except KeyboardInterrupt:
         print "\n\nScript stopped"
         print "Goodbye"
